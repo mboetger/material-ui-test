e880b3efca47c853067c61294c018ace85bbcd3e
mboetger <matt.boetger@gmail.com>
2014-11-15 15:25:34 -0800

    Initial Express/Material UI Testing

---
+++ app/bootstrap.js
@@ -0,0 +1,12 @@
+/**
+ ** @jsx React.DOM
+ **/
+
+var React = require('react'),
+      mui = require('material-ui'),
+        PaperButton = mui.PaperButton;
+
+React.render(
+  <PaperButton type={PaperButton.Types.FLAT} label="Default" />,
+  document.getElementById('example')
+    );
